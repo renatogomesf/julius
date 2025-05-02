0468b927a824e2b9ce7346d7f0677fd87e2c07c7
renatogomesf <renato_gfe@hotmail.com>
2025-05-02 11:56:12 -0300

    grafico

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,65 +1,61 @@
+import { AreaChartGradient } from '@/components/areaChart/AreaChartGradient';
+
 export default function Overview() {
+  const cardsData = [
+    {
+      title: 'Ganhos totais',
+      value: '1212,00',
+      porcentagem: '+11.01%',
+    },
+    {
+      title: 'Despesas totais',
+      value: '112,00',
+      porcentagem: '+11.01%',
+    },
+    {
+      title: 'Me devem no total',
+      value: '321,00',
+      porcentagem: '+11.01%',
+    },
+    {
+      title: 'Eu devo no total',
+      value: '12,00',
+      porcentagem: '+11.01%',
+    },
+  ];
+
   return (
-    <div>
-      <div className="">
+    <div className="">
+      <div className="px-2.5">
         <h1 className="mt-8 mb-5 font-semibold">Visão geral</h1>
-        <div className="flex max-[790px]:flex-col gap-7">
-          <div className="max-w-[300px] p-6 bg-[#E6F1FD] rounded-2xl">
-            <p className="text-[0.9rem] mb-3">Ganhos totais</p>
-            <div className="flex items-center justify-center gap-5">
-              <div>
-                <span>R$</span>
-                <span className="text-2xl font-semibold">212,00</span>
+        <div className="flex flex-wrap items-start justify-start gap-7">
+          {cardsData.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="w-fit max-[790px]:w-full max-[790px]:max-w-[350px] max-[790px]:m-auto p-6 bg-[#E6F1FD] rounded-2xl"
+              >
+                <p className="text-[0.9rem] mb-3">{item.title}</p>
+                <div className="flex items-center justify-start gap-5">
+                  <div>
+                    <span>R$</span>
+                    <span className="text-2xl font-semibold">{item.value}</span>
+                  </div>
+                  <div>
+                    <span className="text-[0.75rem] mr-2">
+                      {item.porcentagem}
+                    </span>
+                    <span>icon</span>
+                  </div>
+                </div>
               </div>
-              <div>
-                <span className="text-[0.75rem] mr-2">+11.01%</span>
-                <span>icon</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="max-w-[300px] p-6 bg-[#EDEEFC] rounded-2xl">
-            <p className="text-[0.9rem] mb-3">Despesas totais</p>
-            <div className="flex items-center justify-center gap-5">
-              <div>
-                <span>R$</span>
-                <span className="text-2xl font-semibold">111,70</span>
-              </div>
-              <div>
-                <span className="text-[0.75rem] mr-2">+3.39%</span>
-                <span>icon</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="max-w-[300px] p-6 bg-[#EDEEFC] rounded-2xl">
-            <p className="text-[0.9rem] mb-3">Me devem no total</p>
-            <div className="flex items-center justify-center gap-5">
-              <div>
-                <span>R$</span>
-                <span className="text-2xl font-semibold">321,70</span>
-              </div>
-              <div>
-                <span className="text-[0.75rem] mr-2">+3.39%</span>
-                <span>icon</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="max-w-[300px] p-6 bg-[#EDEEFC] rounded-2xl">
-            <p className="text-[0.9rem] mb-3">Eu devo no total</p>
-            <div className="flex items-center justify-center gap-5">
-              <div>
-                <span>R$</span>
-                <span className="text-2xl font-semibold">23,70</span>
-              </div>
-              <div>
-                <span className="text-[0.75rem] mr-2">+3.39%</span>
-                <span>icon</span>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
+      </div>
+
+      <div className="px-2.5 w-full mt-5">
+        <AreaChartGradient />
       </div>
     </div>
   );
