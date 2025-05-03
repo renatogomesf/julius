03095e62c63e0fddb2d9b72431acd4428c7c1cbb
renatogomesf <renato_gfe@hotmail.com>
2025-05-03 12:40:04 -0300

    metas

---
+++ src/app/(pages)/dashboard/metas/columnsMeta.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+import { Badge } from '@/components/ui/badge';
+import { Progress } from '@/components/ui/progress';
+
+import { MoreHorizontal, Pencil, Trash } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export type MetasType = {
+  id: string;
+  meta: string;
+  valorAtual: number;
+  valorTotal: number;
+  tag: any;
+  progress?: any;
+};
+
+export const columnsMeta: ColumnDef<MetasType>[] = [
+  {
+    accessorKey: 'id',
+    header: 'Ações',
+    cell: ({ row }) => {
+      // const meta = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="cursor-pointer">
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {/* <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(meta.id)}
+            >
+              Copy meta ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator /> */}
+            <DropdownMenuItem className="flex justify-between cursor-pointer">
+              Atualizar <Pencil />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex justify-between cursor-pointer">
+              Deletar <Trash />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+  {
+    accessorKey: 'meta',
+    header: 'Meta',
+  },
+  {
+    accessorKey: 'valorAtual',
+    header: 'Valor Atual',
+    cell: ({ row }) => {
+      const valorAtual = parseFloat(row.getValue('valorAtual'));
+      const formatted = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(valorAtual);
+
+      return <div className="font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: 'valorTotal',
+    header: 'Valor Total',
+    cell: ({ row }) => {
+      const valorTotal = parseFloat(row.getValue('valorTotal'));
+      const formatted = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(valorTotal);
+
+      return <div className="font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: 'tag',
+    header: 'Tag',
+    cell: ({ row }) => {
+      const tag: any = row.getValue('tag');
+      return <Badge variant="outline">{tag ? tag : ''}</Badge>;
+    },
+  },
+  {
+    accessorKey: 'progress',
+    header: 'Progresso',
+    cell: ({ row }) => {
+      const valorAtual: number = row.getValue('valorAtual');
+      const valorTotal: number = row.getValue('valorTotal');
+
+      const progress: any =
+        valorAtual && valorTotal ? (valorAtual / valorTotal) * 100 : '';
+      return (
+        <div className="flex items-center gap-2">
+          <span>{progress}%</span>
+          <Progress className="border-1 border-black" value={progress} />
+        </div>
+      );
+    },
+  },
+];
--- src/app/(pages)/dashboard/metas/page.tsx
+++ src/app/(pages)/dashboard/metas/page.tsx
@@ -1,7 +1,60 @@
-export default function Metas() {
+import { MetasType, columnsMeta } from './columnsMeta';
+
+import { DataTable } from '@/components/table/DataTable';
+
+import { Badge } from '@/components/ui/badge';
+
+async function getData(): Promise<MetasType[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      meta: 'camprar um carro',
+      valorAtual: 13000,
+      valorTotal: 50000,
+      tag: 'pessoal',
+    },
+    {
+      id: '728ed52f',
+      meta: 'casar daqui a 2 meses',
+      valorAtual: 5000,
+      valorTotal: 10000,
+      tag: 'pessoal',
+    },
+    {
+      id: '728ed52f',
+      meta: 'viajar',
+      valorAtual: 2000,
+      valorTotal: 10000,
+      tag: 'pessoal',
+    },
+    {
+      id: '728ed52f',
+      meta: 'moto',
+      valorAtual: 5000,
+      valorTotal: 20000,
+      tag: 'pessoal',
+    },
+    {
+      id: '728ed52f',
+      meta: 'terreno',
+      valorAtual: 5000,
+      valorTotal: 100000,
+      tag: 'pessoal',
+    },
+    // ...
+  ];
+}
+
+export default async function Metas() {
+  const data = await getData();
+
   return (
-    <div>
-      <h1>vamos começar Metas</h1>
+    <div className="w-full px-2.5">
+      <h1 className="mt-8 mb-5 font-semibold">Metas</h1>
+      <div className="container mx-auto">
+        <DataTable columns={columnsMeta} data={data} />
+      </div>
     </div>
   );
 }
